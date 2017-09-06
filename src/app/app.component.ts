e45472869dd0f09649f6472e6ebd451823452f54
Sunil Kumar T <s1728k@gmail.com>
2017-09-06 08:01:28 -0700

    first improved commit to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core';;
+declare const Excel: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  menus:{}[] = [
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  {name:'first', icon:'fa-home'},
+  ];
+
+  firstMacro1(){
+  	Excel.run(async (context) => {
+      const range = context.workbook.getSelectedRange();
+      range.format.fill.color = 'blue';
+      // this.ListAllSheetNames(context);
+      await context.sync();
+    });
+  }
+
+  firstMacro(ctx) {
+    Excel.run(async (ctx) => {
+      // let sheet = ctx.workbook.worksheets.getActiveWorksheet();
+      // let values = [[]];
+      //ctx.workbook.worksheets
+      // const range = context.workbook.getSelectedRange();
+      // range.format.fill.color = 'blue';
+      // const AR = context.workbook.getCell().row;
+      // const AC = context.workbook.getCell().column;
+      // const AS = context.workbook.getActiveWorksheet();
+      // const AS1 = context.workbook.
+      // for (let sheet of ctx.workbook.worksheets) {
+      //   values[0].push(sheet.name);
+      // }
+      // values[0].push(1);
+      // let range = sheet.getRange("A1:A1");
+      // range.values = values;
+
+      // let values = [["Product"]];
+      //
+      // //Queue a command to write the sample data to the specified range
+      // //in the worksheet and bold the header row
+      // let range = sheet.getRange("A1");
+      // range.values = values;
+
+      const range = ctx.workbook.getSelectedRange();
+      let v = [[]];
+      for (let i = 0; i < 1000; i++){
+        v.push([]);
+        for (let j = 0; j < 1000; j++){
+          v[i].push(j);
+        }
+      }
+      range.values = v;
+
+      await ctx.sync();
+    });
+  }
+
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,6 +33,7 @@
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
+import 'mdn-polyfills/Object.assign';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -40,6 +41,9 @@
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
+import "core-js/client/shim"; // or load it before other angular2 & zone.js stuff
+import "zone.js";
+import "reflect-metadata";
 
 
 /**
